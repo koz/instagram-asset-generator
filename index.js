fdab7c5897ced9d23c92b2c2b84509ef51f27fa9
Gabriel Kozlowaski <gabriel.kozlowaski@booking.com>
2022-07-12 20:17:52 +0200

    Add dynamic port for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const nodeHtmlToImage = require('node-html-to-image')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app.get('/image', async (req, res) => {
